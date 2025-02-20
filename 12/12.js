0300bed7c5da7df68320868b6f29a13b32115bb2
ohyeon1002 <skyblueazure4@gmail.com>
2025-02-20 10:26:30 +0900

    폭탄게임 수정-미완

--- 12/12.js
+++ 12/12.js
@@ -1,24 +1,45 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const cols = document.querySelectorAll('.col');
-    const bt = document.querySelector('button');
-    let flag = false;
-
+function clicker(cols, flag, bomb) {
+    let count=0;
     for (let [idx, col] of cols.entries()) {
-        if (!flag) {
+        while (!flag) {
             col.innerHTML = idx + 1;
             col.addEventListener('click', () => {
                 col.setAttribute('style', "background-color : paleturquoise;");
                 document.getElementById('noti').innerHTML = `<span style="font-weight:bolder; font-size: 30px;">버튼 누르세요</ span>`
+                return;
+            });
+            break;
+        }
+        if (flag) {
+            col.innerHTML = null;
+            col.setAttribute('style', null);
+            col.addEventListener('click', ()=>{
+                count++;
+                console.log(count);
+                if (idx == bomb & count < 8) {
+                    col.innerHTML = `<img src="../img/boom.png" style="width:70px; height:70px;">`;
+                }
+                else if(idx != bomb) col.innerHTML = `<img src="../img/hart.png" style="width:70px; height:70px;">`;
+                else if(idx == bomb & count == 9) {
+                    col.innerHTML = `<img src="../img/hart.png" style="width:70px; height:70px;">`;
+                    document.getElementById('noti').innerHTML = `<span style="font-weight:bolder; font-size: 30px;">성공!</ span>`;
+                }
             });
         }
-        else if (flag) {
-            col.innerHTML = null;// 하트 or 폭탄 이미지 삽입
-        }
     }
+}
 
+document.addEventListener('DOMContentLoaded', () => {
+    const cols = document.querySelectorAll('.col');
+    const bt = document.querySelector('button');
+    let flag = false;
+    let bomb;
     bt.addEventListener('click', () => {
+        document.getElementById('noti').textContent = '';
         flag = true;
-        let bomb = Math.floor(Math.random() * 9 + 1);
-        
+        bomb = Math.floor(Math.random() * 8 + 1);
+        console.log(bomb+1);
+        clicker(cols, flag, bomb);
     });
+    clicker(cols, flag, bomb);
 });
